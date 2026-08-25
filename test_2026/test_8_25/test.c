d51f7c3538a5d0156fd766bc8ece535246dea7e5
GRBQF <3257948923@qq.com>
2026-08-25 19:57:52 +0800

    study

---
+++ test_2026/test_8_25/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//std--standard
+//i--input
+//o--output
+//.h --头文件
+
+int main()
+{
+	/*printf("%c\n", 100);
+	printf("%lf\n", 3.5);
+	printf("%f\n", 3.5f);
+
+	int a = 111; 
+	char b = 'a';
+	printf("a = %d ,b = %d\n", a, b);
+	printf("a = %c ,b = %c\n", a, b);
+
+	printf("%c\n", 'A');
+	printf("%c\n", 'A' + 32);*/
+
+	/*for (int i = 32; i <= 127; i++)
+	{
+		printf("%c  ", i);
+	}*/
+	//0--数字0
+	//'0'--字符0  ASCII值
+
+	'a';
+	"abcdef";
+	printf("%c\n", 'a');
+	printf("%s\n", "abcdefg");
+	printf("yes");
+	char arr1[] = { 'a','b','c' };
+	printf("%s\n", arr1);
+	printf("%c", '\'');
+	char a = "a";
+	printf("\a");
+	printf("djasklfjsa\r ");
+	return 0;
+}
